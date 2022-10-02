c6685c156d2e0013a7a15a52983c566ef27d4c17
Raimo Radczewski <raimoradczewski@googlemail.com>
2022-10-02 22:21:48 +0200

    Try things with users and logging

--- .bin/run_test_in_devcontainer.sh
+++ .bin/run_test_in_devcontainer.sh
@@ -5,8 +5,8 @@ set -xeuo pipefail
 run() {
     local DIR="$1"
 
-    devcontainer up --remove-existing-container --workspace-folder="$DIR"
-    devcontainer run-user-commands --workspace-folder="$DIR"
+    devcontainer up --log-level=trace --remove-existing-container --workspace-folder="$DIR"
+    devcontainer run-user-commands --log-level=trace --workspace-folder="$DIR"
 }
 
 run "$1"
