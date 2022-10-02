4df962970cdef96a6ef4fb57f2ecc9fba1059ab2
Raimo Radczewski <hello@craftswerk.io>
2022-10-02 19:30:30 +0200

    Remove tasks.json, use postCreateCommand for validation:

--- .bin/run_test_in_devcontainer.sh
+++ .bin/run_test_in_devcontainer.sh
@@ -7,8 +7,6 @@ run() {
 
     devcontainer up --remove-existing-container --workspace-folder="$DIR"
     devcontainer run-user-commands --workspace-folder="$DIR"
-    local TESTCOMMAND=$(jq -r '.tasks[] | select(.label == "Validate Bootstrap") | .command' "$DIR/.vscode/tasks.json")
-    devcontainer exec --workspace-folder="$DIR" bash -x -c "$TESTCOMMAND"
 }
 
 run "$1"
