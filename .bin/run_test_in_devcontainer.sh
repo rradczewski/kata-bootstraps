eea3ae7ba93b971739d5b11ea7162bcedbed1c72
Raimo Radczewski <hello@craftswerk.io>
2022-10-07 18:20:54 +0200

    Move all testVerification commands into their own namespace so that they don't interfere with container creation

--- .bin/run_test_in_devcontainer.sh
+++ .bin/run_test_in_devcontainer.sh
@@ -6,6 +6,13 @@ run() {
     local DIR="$1"
 
     devcontainer run-user-commands --log-level=trace --workspace-folder="$DIR"
+
+    local TEST_COMMAND=$(devcontainer read-configuration --log-level=trace --workspace-folder="$DIR" |
+        jq -r '.configuration.customizations["github.com/rradczewski/kata-bootstraps"].failingTestVerification // empty')
+    if [ ! -z "${TEST_COMMAND}" ]; then
+        devcontainer exec --log-level=trace --workspace-folder="$DIR" bash -c "${TEST_COMMAND}"
+    fi
+
     devcontainer exec --log-level=trace --workspace-folder="$DIR" bash -c 'echo $UID:$GID; whoami'
 }
 
