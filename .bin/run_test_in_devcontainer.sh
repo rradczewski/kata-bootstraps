e46b69893c77a54364e047e38a21e006958ac3a8
Raimo Radczewski <hello@craftswerk.io>
2022-10-07 18:49:27 +0200

    Rearrange scripts to reduce noise in CI output

--- .bin/run_test_in_devcontainer.sh
+++ .bin/run_test_in_devcontainer.sh
@@ -5,15 +5,9 @@ set -xeuo pipefail
 run() {
     local DIR="$1"
 
-    devcontainer run-user-commands --log-level=trace --workspace-folder="$DIR"
-
     local TEST_COMMAND=$(devcontainer read-configuration --log-level=trace --workspace-folder="$DIR" |
-        jq -r '.configuration.customizations["github.com/rradczewski/kata-bootstraps"].failingTestVerification // empty')
-    if [ ! -z "${TEST_COMMAND}" ]; then
-        devcontainer exec --log-level=trace --workspace-folder="$DIR" bash -c "${TEST_COMMAND}"
-    fi
-
-    devcontainer exec --log-level=trace --workspace-folder="$DIR" bash -c 'echo $UID:$GID; whoami'
+        jq -e -r '.configuration.customizations["github.com/rradczewski/kata-bootstraps"].failingTestVerification')
+    devcontainer exec --log-level=trace --workspace-folder="$DIR" bash -c "${TEST_COMMAND}"
 }
 
 run "$1"
