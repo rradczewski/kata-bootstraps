589b639871203484d1ca10dfcf80aa60313b889b
Raimo Radczewski <hello@craftswerk.io>
2023-01-26 10:57:34 +0100

    [meta] Use branches instead of merging devcontainers

--- .bin/update_and_force_push_language_branches.sh
+++ .bin/update_and_force_push_language_branches.sh
@@ -2,9 +2,8 @@
 
 set -euxo pipefail
 
-PATH="$( dirname -- "$0" )/node_modules/.bin/:${PATH}"
+PATH="$(dirname -- "$0")/node_modules/.bin/:${PATH}"
 ROOT_DIR=$(readlink -f "$(dirname "$0")/../")
-
 
 transform_devcontainer() {
     local DIR="$1"
@@ -12,11 +11,10 @@ transform_devcontainer() {
     local CONFIG=$(devcontainer read-configuration --workspace-folder="$DIR")
 
     local FILE=$(echo "$CONFIG" | jq -r .configuration.configFilePath.path)
-    
+
     local DOCKERFILE=$(echo "$CONFIG" | jq -r '.configuration.build.dockerfile // ""')
 
-    if [ ! -z "$DOCKERFILE" ];
-    then
+    if [ ! -z "$DOCKERFILE" ]; then
         local FILE_DIR=$(dirname "$FILE")
         DOCKERFILE=$(realpath --relative-to "$ROOT_DIR" "$FILE_DIR/$DOCKERFILE")
     fi
@@ -35,11 +33,17 @@ transform_devcontainer() {
         "$FILE" | tee "$ROOT_DIR/.devcontainer/$DIRNAME/devcontainer.json"
 }
 
+update_language_branch() {
+    local DIR="$1"
+    local DIRNAME=$(basename "$DIR")
+
+    local SPLIT_REF=$(git subtree split --prefix "$DIRNAME/" "main")
+    git push origin "${SPLIT_REF}:refs/heads/${DIRNAME}" --force
+}
 
 run() {
-    for dir in $(ls -d $ROOT_DIR/*/);
-    do
-        transform_devcontainer "$dir"
+    for dir in $(ls -d $ROOT_DIR/*/); do
+        update_language_branch "$dir"
     done
 }
 
