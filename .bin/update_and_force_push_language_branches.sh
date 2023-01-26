b95d8b699ce3ba3e4286c3e2e9950fea48aa49ce
Raimo Radczewski <rradczewski@users.noreply.github.com>
2023-01-26 15:11:53 +0100

    [meta] Remove old function

--- .bin/update_and_force_push_language_branches.sh
+++ .bin/update_and_force_push_language_branches.sh
@@ -5,34 +5,6 @@ set -euxo pipefail
 PATH="$(dirname -- "$0")/node_modules/.bin/:${PATH}"
 ROOT_DIR=$(readlink -f "$(dirname "$0")/../")
 
-transform_devcontainer() {
-    local DIR="$1"
-    local DIRNAME=$(basename "$DIR")
-    local CONFIG=$(devcontainer read-configuration --workspace-folder="$DIR")
-
-    local FILE=$(echo "$CONFIG" | jq -r .configuration.configFilePath.path)
-
-    local DOCKERFILE=$(echo "$CONFIG" | jq -r '.configuration.build.dockerfile // ""')
-
-    if [ ! -z "$DOCKERFILE" ]; then
-        local FILE_DIR=$(dirname "$FILE")
-        DOCKERFILE=$(realpath --relative-to "$ROOT_DIR" "$FILE_DIR/$DOCKERFILE")
-    fi
-
-    mkdir -p "$ROOT_DIR/.devcontainer/$DIRNAME" || true
-
-    jq \
-        --arg dockerfile "$DOCKERFILE" \
-        '(if($dockerfile | length != 0) then 
-            .build.dockerfile = ("${localWorkspaceFolder}/" + $dockerfile)
-         else
-            . end) |
-         .workspaceFolder = "/workspaces/kata-bootstraps/'${DIRNAME}'" |
-         .workspaceMount = "type=bind,source=${localWorkspaceFolder}/'${DIRNAME}',target=/workspaces/kata-bootstraps/'${DIRNAME}'"
-        ' \
-        "$FILE" | tee "$ROOT_DIR/.devcontainer/$DIRNAME/devcontainer.json"
-}
-
 update_language_branch() {
     local DIR="$1"
     local DIRNAME=$(basename "$DIR")
