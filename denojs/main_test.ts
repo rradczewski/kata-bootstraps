6d287d608554c65cef1e05bdc87e19ccad35d395
Raimo Radczewski <hello@craftswerk.io>
2023-09-13 13:22:37 +0200

    [denojs] Add Deno Docker image to renovate

--- denojs/main_test.ts
+++ denojs/main_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.201.0/assert/mod.ts";
+import { assertEquals } from "https://deno.land/std/assert/mod.ts";
 
 Deno.test(function test_succeeds() {
   assertEquals(true, true);
