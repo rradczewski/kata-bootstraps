620344d7955004861b81cf2a416722b7a386b565
Raimo Radczewski <hello@craftswerk.io>
2024-10-08 09:10:49 +0200

    [meta] Don't delete branches that start with stage/

--- .bin/clean_old_branches.sh
+++ .bin/clean_old_branches.sh
@@ -5,6 +5,7 @@ set -ux
 BRANCHES=$(comm -13 <(ls -d */ | sed 's/.$//' | sort) <(git ls-remote --heads origin | awk '{ print substr($2, 12) }' | sort) |
     grep -v renovate |
     grep -vE '^fix/' |
+    grep -vE '^stage/' |
     grep -v main |
     grep -v gh-pages)
 
